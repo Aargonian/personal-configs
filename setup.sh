65f895584398f066d3feb80b0f6cc257e03824f7
Aaron Helton <aaron@nytework.com>
2022-10-07 20:14:10 -0400

    Blarg

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ if [ "$EUID" -eq 0 ]
 fi
 
 packages=(
+    # Base Packages
+    "base"
+
     # Terminal Programs
     "tmux"
     "screen"
@@ -14,11 +17,21 @@ packages=(
     "gvim"
     "terminator"
 
+    # "Desktop Environment" lol
+    "i3"
     "dmenu"
+    "feh"
+    "nitrogen"
+    "firefox"
+    "xfce4"
+    "xfce4-goodies"
+    "networkmanager"
+    "blueberry"
+
+    # "Development Tools"
     "base-devel"
     "cmake"
     "git"
-    "firefox"
     "rustup"
 )
 
@@ -27,14 +40,14 @@ for package in ${packages[@]}; do
     package_list="$package_list $package"
 done
 
-sudo pacman -Syu
-sudo pacman -Svyu $package_list
+sudo pacman -Syu $package_list
 
 cd $HOME
 mkdir -p ./src/git
 cd ./src/git
-git clone https://github.com/ryanoasis/nerd-fonts
+git clone https://github.com/ryanoasis/nerd-fonts --depth=1
 cd ./nerd-fonts
 ./install.sh
 cd $HOME
 
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
