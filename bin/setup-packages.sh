d18fc1acec1675797c8da0e6ed38c52422432643
Aaron Helton <aaron@nytework.com>
2022-10-09 01:28:59 -0400

    Changes or something

--- bin/setup-packages.sh
+++ bin/setup-packages.sh
@@ -8,6 +8,7 @@ fi
 packages=(
     # Base Packages
     "base"
+    "base-devel"
 
     # Terminal Programs
     "zsh"
@@ -17,23 +18,34 @@ packages=(
     "htop"
     "gvim"
     "terminator"
+    "wget"
+    "curl"
+    "rsync"
 
     # "Desktop Environment" lol
-    "i3"
-    "dmenu"
-    "feh"
-    "nitrogen"
-    "firefox"
-    "xfce4"
-    "xfce4-goodies"
-    "networkmanager"
-    "blueberry"
+    "i3"                # i3 Window Manager
+    "lightdm"           # Login Manager
+    "picom"             # Compositor for Transparency in i3
+    "dmenu"             # Gotta open programs somehow
+    "feh"               # Set a single background. Nitrogen is better though.
+    "nitrogen"          # Set multiple desktop backgrounds.
+    "networkmanager"    # Manager WiFi and Ethernet Networks Easily
+    "blueberry"         # Gotta manage Bluetooth somehow
+    "xautolock"         # Automatic Desktop Locker
 
     # "Development Tools"
     "base-devel"
     "cmake"
     "git"
     "rustup"
+
+    # Desktop Programs
+    "vlc"               # Musie/Video Player
+    "calibre"           # Ebook Library Manager
+    "xreader"           # PDF Reader
+    "firefox"           # Preferred Web Browser
+
+    # Other
 )
 
 package_list=""
